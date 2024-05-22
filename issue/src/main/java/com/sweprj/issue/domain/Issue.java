21cc441a00bea81ca5cde9508d868322ee2ac9a9
bykbyk0401 <bykbyk0401@gmail.com>
2024-05-22 16:07:34 +0900

    setting: 프로젝트 기본 세팅

--- issue/src/main/java/com/sweprj/issue/domain/Issue.java
+++ issue/src/main/java/com/sweprj/issue/domain/Issue.java
@@ -11,7 +11,6 @@ import java.util.Date;
 @Entity
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class Issue {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
