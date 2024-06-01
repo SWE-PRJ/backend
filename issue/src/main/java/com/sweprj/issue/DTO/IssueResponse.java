164e045940b4bc43cc315277ad7261c98708a021
lee-haeseung <lhs020205@naver.com>
2024-06-02 00:27:12 +0900

    mod : fix issue response
    issue response의 재귀 문제 해결

--- issue/src/main/java/com/sweprj/issue/DTO/IssueResponse.java
+++ issue/src/main/java/com/sweprj/issue/DTO/IssueResponse.java
@@ -7,6 +7,7 @@ import com.sweprj.issue.domain.enums.IssueState;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -26,11 +27,11 @@ public class IssueResponse {
         reporterIdentifier = issue.getReporter() != null ? issue.getReporter().getIdentifier() : "";
         fixerIdentifier = issue.getFixer() != null ? issue.getFixer().getIdentifier() : "";
         assigneeIdentifier = issue.getAssignee() != null ? issue.getAssignee().getIdentifier() : "";
-        priority = issue.getPriority() != null ? issue.getPriority() : null; // 필요 시 기본값 설정
-        state = issue.getState() != null ? issue.getState() : IssueState.NEW; // 필요 시 기본값 설정
+        priority = issue.getPriority() != null ? issue.getPriority() : null;
+        state = issue.getState() != null ? issue.getState() : IssueState.NEW;
         projectId = issue.getProject() != null ? issue.getProject().getId() : -1L;
-        reportedAt = issue.getReportedAt() != null ? issue.getReportedAt() : null; // 필요 시 기본값 설정
-        comments = issue.getComments() != null ? issue.getComments() : Collections.emptyList();
+        reportedAt = issue.getReportedAt() != null ? issue.getReportedAt() : null;
+        comments = issue.getComments() != null ? convertToDTO(issue.getComments()) : Collections.emptyList();
     }
 
     private Long id;
@@ -43,5 +44,20 @@ public class IssueResponse {
     private IssueState state;
     private Long projectId;
     private Date reportedAt;
-    private List<Comment> comments;
+    private List<CommentDTO> comments;
+
+    private CommentDTO convertToDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setCommenterId(comment.getCommenter().getUserId());
+        commentDTO.setContent(comment.getContent());
+        commentDTO.setCommentedAt(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(comment.getCommentedAt()));
+        commentDTO.setIssueId(comment.getIssue().getId());
+        return commentDTO;
+    }
+    private List<CommentDTO> convertToDTO(List<Comment> comments) {
+        return comments.stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
 }
