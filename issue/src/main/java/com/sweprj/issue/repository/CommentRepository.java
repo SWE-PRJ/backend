ec2302e22f56c58e5e8d7ff8e655b63526093752
Handoni <sang1234yun@naver.com>
2024-06-03 03:46:50 +0900

    fix: getAllComments 로직 변경

--- issue/src/main/java/com/sweprj/issue/repository/CommentRepository.java
+++ issue/src/main/java/com/sweprj/issue/repository/CommentRepository.java
@@ -4,7 +4,8 @@ import com.sweprj.issue.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByIssueId(Long issueId);
+    Optional<List<Comment>> findByIssueId(Long issueId);
 }
